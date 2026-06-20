73d5d0619aeef56d3dc50389ea84b0416263fb6f
Bara Shaban <barashaban03@gmail.com>
2026-06-20 19:57:00 +0300

    keys update

--- docs/config.js
+++ docs/config.js
@@ -1,1 +1,5 @@
-window.YT_FRONTEND_CONFIG = window.YT_FRONTEND_CONFIG || {};
+window.YT_FRONTEND_CONFIG = window.YT_FRONTEND_CONFIG || {
+    youtubeApiKey: "AIzaSyDac5rlK4OnR6ADbshPpROTDyazS7fTMfs",
+    resolverBase: "https://steelless-tetartohedrally-terina.ngrok-free.dev",
+    resolverKey: "fb66e592dedf3df8797188cef89d9ba1",
+};
